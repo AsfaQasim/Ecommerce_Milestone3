1221f9f93a7335be37c8716e8fdfac3ea605cfe9
AsfaQasim <asfaqasim144@gmail.com>
2025-01-04 00:54:11 +0500

    main

--- src/app/component/navbar.tsx
+++ src/app/component/navbar.tsx
@@ -93,7 +93,7 @@ const Navbar = () => {
         <div className="flex md:hidden">
           <Sheet>
             <SheetTrigger>
-              <svg
+              {/* <svg
                 xmlns="http://www.w3.org/2000/svg"
                 height="40px"
                 viewBox="0 -960 960 960"
@@ -102,7 +102,7 @@ const Navbar = () => {
                 className="ml-[22rem]"
               >
                 <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
-              </svg>
+              </svg> */}
             </SheetTrigger>
             <SheetContent>
               <SheetHeader>
